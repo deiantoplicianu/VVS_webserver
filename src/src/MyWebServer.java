3e5a148f2ec2f23f1819b44d7c89552fd31c6a97
Deian <deian.toplicianu@gamil.com>
2021-12-22 01:17:28 +0200

    add some files

---
+++ src/src/MyWebServer.java
@@ -0,0 +1,56 @@
+package src;
+
+import exceptions.PortOutOfBoundException;
+import exceptions.UsedPortException;
+import gui.GUI;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+import static org.junit.Assert.assertArrayEquals;
+
+public class MyWebServer extends Thread{
+
+    protected Socket clientSocket;
+    
+    static Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    
+    
+
+    public static void main(String[] args) throws IOException {
+        GUI.main(null);
+    	
+    	ConfigManager configManager = new ConfigManager(new Configuration());
+    	
+
+        try {
+            ServerSocket serverConnect = new ServerSocket(configManager.getPort());
+            System.out.println("Server started.\nListening for connections on port : " + configManager.getPort());
+
+            // we listen until user halts server execution
+            while (true) {
+                
+            	WebServer myServer = new WebServer(serverConnect.accept(), configManager);      	
+                configManager.setState(GUI.text);
+
+                if (Configuration.verbose) {
+                    System.out.println("Connecton opened. (" + new Date() + ")");
+                }
+
+                // create dedicated thread to manage the client connection
+                Thread thread = new Thread(myServer);
+                thread.start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Server Connection error : " + e.getMessage());
+        }
+    }
+
+	
+
+}
